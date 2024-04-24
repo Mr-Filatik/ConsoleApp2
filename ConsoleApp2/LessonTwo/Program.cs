e0a6320d87f3fd943305e477045c49b903cf9f3f
Vladislav Filatov <v.filatov@softchamber.ru>
2024-04-24 20:27:34 +0400

    Update Program in LessonTwo

--- ConsoleApp2/LessonTwo/Program.cs
+++ ConsoleApp2/LessonTwo/Program.cs
@@ -32,6 +32,70 @@
             admin.SuperPassword = "Test";
 
             Console.WriteLine();
+
+            People pp = new People() { Age = 1 };
+            People pu = new User() { Age = 2 };
+            People pa = new Admin() { Age = 3 };
+            //User up = new People();
+            User uu = new User() { Age = 4 };
+            User ua = new Admin() { Age = 5 };
+            //Admin ap = new People();
+            //Admin au = new User();
+            Admin aa = new Admin() { Age = 6 };
+
+            People[] peoples = { pp, pu, pa, uu, ua, aa };
+            foreach (var item in peoples)
+            {
+                GetInfo(item);
+            }
+        }
+
+        internal static void GetInfo(People people)
+        {
+            //Admin admin = (Admin)people;
+
+            // 1
+            //if (people is Admin)
+            //{
+            //    Console.WriteLine("Admin");
+            //    var admin = (Admin)people; //!!!!
+            //    Console.WriteLine(admin.SuperPassword);
+            //}
+
+            // 2
+            //Admin admin = people as Admin;
+            //if (admin != null)
+            //{
+            //    Console.WriteLine("Admin");
+            //}
+
+            //User user = people as User;
+            //if (user != null)
+            //{
+            //    Console.WriteLine("User");
+            //}
+
+            //if (people != null)
+            //{
+            //    Console.WriteLine("People");
+            //}
+
+            // 3
+            //switch (people)
+            //{
+            //    case Admin:
+            //        Console.WriteLine("Admin");
+            //        var admin = (Admin)people; //!!!!
+            //        Console.WriteLine(admin.SuperPassword);
+            //        break;
+            //    case User:
+            //        Console.WriteLine("User");
+            //        break;
+            //    default:
+            //        Console.WriteLine("People");
+            //        break;
+            //}
+            Console.WriteLine(people.Age);
         }
     }
 }
